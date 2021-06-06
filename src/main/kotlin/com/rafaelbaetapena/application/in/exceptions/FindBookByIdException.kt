8766b43f6a9a66ae63caab5f614caa7d3e7cb168
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-06-06 18:12:53 -0300

    adicioanndo o teste unitário da busca de livos por id

--- src/main/kotlin/com/rafaelbaetapena/application/in/exceptions/FindBookByIdException.kt
+++ src/main/kotlin/com/rafaelbaetapena/application/in/exceptions/FindBookByIdException.kt
@@ -1,6 +1,4 @@
 package com.rafaelbaetapena.application.`in`.exceptions
 
 class FindBookByIdException(override val message: String = "No book was found for the Id entered"):
-        Exception(message) {
-
-}
+        Exception(message)
---
+++ src/test/kotlin/com/rafaelbaetapena/application/in/usecases/impl/FindBookByIdUseCaseImplTest.kt
@@ -0,0 +1,53 @@
+package com.rafaelbaetapena.application.`in`.usecases.impl
+
+import com.rafaelbaetapena.application.`in`.domain.Book
+import com.rafaelbaetapena.application.`in`.domain.BookCategory
+import com.rafaelbaetapena.application.`in`.exceptions.FindBookByIdException
+import com.rafaelbaetapena.application.out.port.FindBookByIdPort
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.slf4j.LoggerFactory
+import java.util.*
+
+@ExtendWith(MockitoExtension::class)
+internal class FindBookByIdUseCaseImplTest{
+
+    companion object{
+        private val log = LoggerFactory.getLogger(FindBookByIdUseCaseImplTest::class.java)
+    }
+
+    @Mock
+    lateinit var findBookByIdPort: FindBookByIdPort
+
+    @InjectMocks
+    lateinit var findBookByIdUseCaseImpl: FindBookByIdUseCaseImpl
+
+    @Test
+    fun `given an existing book id then the book data should be returning`() {
+
+        val id = UUID.fromString("f680eba8-d0ab-4a95-8ec7-ee6ed4716606")
+        val book = Book(id,"O Hobbit",
+                "J.R.R. Tolkien", "HarperCollins", 336, BookCategory.FANTASY)
+        Mockito.`when`(findBookByIdPort.execute(id)).thenReturn(book)
+
+        val actual = findBookByIdUseCaseImpl.execute(id)
+        assertNotNull(actual)
+        assertEquals(book, actual)
+        log.info("Returned book: $actual")
+    }
+
+    @Test
+    fun `given a non-existent book id then it should return an exception`() {
+
+        val id = UUID.fromString("f680eba8-d0ab-4a95-8ec7-ee6ed4716606")
+        Mockito.`when`(findBookByIdPort.execute(id)).thenReturn(null)
+
+        val assertThrows = assertThrows(FindBookByIdException::class.java) { findBookByIdUseCaseImpl.execute(id) }
+        log.info("Returned book: $assertThrows")
+    }
+}
