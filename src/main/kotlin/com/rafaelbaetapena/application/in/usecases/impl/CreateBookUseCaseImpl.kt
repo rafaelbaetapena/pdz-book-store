b21628537dce6ba5295e8d0a80fe9a83f266e309
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-06-06 14:27:33 -0300

    adicionando o singleton

--- src/main/kotlin/com/rafaelbaetapena/application/in/usecases/impl/CreateBookUseCaseImpl.kt
+++ src/main/kotlin/com/rafaelbaetapena/application/in/usecases/impl/CreateBookUseCaseImpl.kt
@@ -3,7 +3,9 @@ package com.rafaelbaetapena.application.`in`.usecases.impl
 import com.rafaelbaetapena.application.`in`.domain.Book
 import com.rafaelbaetapena.application.`in`.usecases.CreateBookUseCase
 import com.rafaelbaetapena.application.out.port.CreateBookPort
+import javax.inject.Singleton
 
+@Singleton
 class CreateBookUseCaseImpl(private val createBookPort: CreateBookPort):
         CreateBookUseCase {
 
