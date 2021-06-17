0a89a8a45bf987f20eb239a971c6914e0afa820e
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-06-16 21:25:10 -0300

    adicionando a implementação do CreateBookAdapter

--- src/main/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/CreateBookAdapterImpl.kt
+++ src/main/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/CreateBookAdapterImpl.kt
@@ -1,10 +1,16 @@
 package com.rafaelbaetapena.adapters.out.persistence.v1
 
+import com.rafaelbaetapena.adapters.out.persistence.v1.entities.BookEntity
+import com.rafaelbaetapena.adapters.out.persistence.v1.repositories.BookRepository
 import com.rafaelbaetapena.application.domain.Book
 import com.rafaelbaetapena.application.port.out.CreateBookAdapter
 import javax.inject.Singleton
 
 @Singleton
-class CreateBookAdapterImpl: CreateBookAdapter {
-    override fun execute(book: Book): Book = book
+class CreateBookAdapterImpl(
+    private val bookRepository: BookRepository
+): CreateBookAdapter {
+
+    override fun execute(book: Book): Book =
+            bookRepository.save(BookEntity(book)).toDomain()
 }
---
+++ src/test/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/CreateBookAdapterImplTest.kt
@@ -0,0 +1,47 @@
+package com.rafaelbaetapena.adapters.out.persistence.v1
+
+import com.rafaelbaetapena.adapters.out.persistence.v1.entities.BookEntity
+import com.rafaelbaetapena.adapters.out.persistence.v1.repositories.BookRepository
+import com.rafaelbaetapena.application.domain.Book
+import com.rafaelbaetapena.application.domain.BookCategory
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.slf4j.LoggerFactory
+
+@ExtendWith(MockitoExtension::class)
+internal class CreateBookAdapterImplTest {
+
+    companion object{
+        private val log = LoggerFactory.getLogger(CreateBookAdapterImplTest::class.java)
+    }
+
+    @Mock
+    lateinit var bookRepository: BookRepository
+
+    @InjectMocks
+    lateinit var createBookAdapterImpl: CreateBookAdapterImpl
+
+    @Test
+    fun `given a new book when the user enters all the book data correctly then the book is successfully registered`() {
+
+        val book = Book(
+                name = "The Hobbit",
+                author = "J.R.R. Tolkien",
+                publisher = "HarperCollins Publishers",
+                numberOfPages = 400,
+                category = BookCategory.FANTASY)
+        val bookEntity = BookEntity(book)
+        Mockito.`when`(bookRepository.save(bookEntity)).thenReturn(bookEntity)
+
+        val actual = createBookAdapterImpl.execute(book)
+        assertNotNull(actual)
+        assertEquals(book, actual)
+        assertNotNull(actual.id)
+        log.info("Book created: $actual")
+    }
+}
