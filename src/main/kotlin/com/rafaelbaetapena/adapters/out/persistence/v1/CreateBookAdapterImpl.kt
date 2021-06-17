90aa69f1070d6bbb9ad413eb260ad632b0eda3cb
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-06-16 21:38:32 -0300

    adicionando logs

--- src/main/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/CreateBookAdapterImpl.kt
+++ src/main/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/CreateBookAdapterImpl.kt
@@ -4,6 +4,8 @@ import com.rafaelbaetapena.adapters.out.persistence.v1.entities.BookEntity
 import com.rafaelbaetapena.adapters.out.persistence.v1.repositories.BookRepository
 import com.rafaelbaetapena.application.domain.Book
 import com.rafaelbaetapena.application.port.out.CreateBookAdapter
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import javax.inject.Singleton
 
 @Singleton
@@ -11,6 +13,18 @@ class CreateBookAdapterImpl(
     private val bookRepository: BookRepository
 ): CreateBookAdapter {
 
-    override fun execute(book: Book): Book =
-            bookRepository.save(BookEntity(book)).toDomain()
+    override fun execute(book: Book): Book {
+
+        log.info("$CLASS_NAME starting create book")
+
+        val createdBook = bookRepository.save(BookEntity(book)).toDomain()
+
+        log.info("$CLASS_NAME finalized create book")
+
+        return  createdBook
+    }
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(CreateBookAdapterImpl::class.java)
+        private val CLASS_NAME = "[${CreateBookAdapterImpl::class.java}]"
+    }
 }
--- src/main/kotlin/com/rafaelbaetapena/application/port/in/impl/CreateBookUseCaseImpl.kt
+++ src/main/kotlin/com/rafaelbaetapena/application/port/in/impl/CreateBookUseCaseImpl.kt
@@ -3,6 +3,8 @@ package com.rafaelbaetapena.application.port.`in`.impl
 import com.rafaelbaetapena.application.domain.Book
 import com.rafaelbaetapena.application.port.`in`.CreateBookUseCase
 import com.rafaelbaetapena.application.port.out.CreateBookAdapter
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import javax.inject.Singleton
 
 @Singleton
@@ -10,6 +12,18 @@ class CreateBookUseCaseImpl(private val createBookAdapter: CreateBookAdapter):
         CreateBookUseCase {
 
     override fun execute(book: Book): Book {
-        return createBookAdapter.execute(book)
+
+        log.info("$CLASS_NAME starting create book")
+
+        val createdBook = createBookAdapter.execute(book)
+
+        log.info("$CLASS_NAME finalized create book")
+
+        return createdBook
+    }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(CreateBookUseCaseImpl::class.java)
+        private val CLASS_NAME = "[${CreateBookUseCaseImpl::class.java}]"
     }
 }
