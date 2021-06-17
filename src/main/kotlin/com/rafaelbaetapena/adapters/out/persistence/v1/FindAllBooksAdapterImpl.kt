5bad4bca03d8f5c12b78db6d6d0a185e27c99985
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-06-17 19:19:14 -0300

    implementando o adapter

---
+++ src/main/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/FindAllBooksAdapterImpl.kt
@@ -0,0 +1,28 @@
+package com.rafaelbaetapena.adapters.out.persistence.v1
+
+import com.rafaelbaetapena.adapters.out.persistence.v1.repositories.BookRepository
+import com.rafaelbaetapena.application.domain.Book
+import com.rafaelbaetapena.application.domain.BookFilter
+import com.rafaelbaetapena.application.port.out.FindAllBooksAdapter
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class FindAllBooksAdapterImpl(
+        private val bookRepository: BookRepository
+): FindAllBooksAdapter {
+
+    override fun execute(filters: BookFilter): List<Book> {
+
+        log.info("$CLASS_NAME starting find all books")
+
+        val allBooks = bookRepository.findByFilters(filters).map { it.toDomain() }
+
+        log.info("$CLASS_NAME finalized find all books")
+
+        return  allBooks
+    }
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(FindAllBooksAdapterImpl::class.java)
+        private val CLASS_NAME = "[${FindAllBooksAdapterImpl::class.java}]"
+    }
+}
---
+++ src/test/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/FindAllBooksAdapterImplTest.kt
@@ -0,0 +1,58 @@
+package com.rafaelbaetapena.adapters.out.persistence.v1
+
+import com.rafaelbaetapena.adapters.out.persistence.v1.entities.BookEntity
+import com.rafaelbaetapena.adapters.out.persistence.v1.repositories.BookRepository
+import com.rafaelbaetapena.application.domain.Book
+import com.rafaelbaetapena.application.domain.BookCategory
+import com.rafaelbaetapena.application.domain.BookFilter
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
+import org.slf4j.LoggerFactory
+import java.util.*
+
+@ExtendWith(MockitoExtension::class)
+internal class FindAllBooksAdapterImplTest {
+
+    companion object{
+        private val log = LoggerFactory.getLogger(FindAllBooksAdapterImplTest::class.java)
+    }
+
+    @Mock
+    lateinit var bookRepository: BookRepository
+
+    @InjectMocks
+    lateinit var findAllBooksAdapterImpl: FindAllBooksAdapterImpl
+
+    @Test
+    fun `given the filter values then returns all found books`() {
+
+        val bookEntity = BookEntity(
+                id = UUID.randomUUID(),
+                name = "The Hobbit",
+                author = "J.R.R. Tolkien",
+                publisher = "HarperCollins Publishers",
+                numberOfPages = 400,
+                category = BookCategory.FANTASY)
+
+        val filters = BookFilter(
+                name = "The Hobbit",
+                author = "J.R.R. Tolkien",
+                publisher = "HarperCollins Publishers",
+                category = BookCategory.FANTASY
+        )
+
+        whenever(bookRepository.findByFilters(filters)).thenReturn(listOf(bookEntity))
+
+        val actual = findAllBooksAdapterImpl.execute(filters)
+        assertNotNull(actual)
+        assertFalse(actual.isEmpty())
+        assertEquals(1, actual.size)
+        log.info("Book found: $actual")
+    }
+}
