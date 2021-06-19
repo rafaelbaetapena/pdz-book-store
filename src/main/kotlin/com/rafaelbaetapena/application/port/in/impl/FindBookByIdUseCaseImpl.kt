4944f3f027e82c0fb1ba31d5183bc830ad735179
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-06-18 23:25:56 -0300

    adicionando logs

--- src/main/kotlin/com/rafaelbaetapena/application/port/in/impl/FindBookByIdUseCaseImpl.kt
+++ src/main/kotlin/com/rafaelbaetapena/application/port/in/impl/FindBookByIdUseCaseImpl.kt
@@ -4,14 +4,29 @@ import com.rafaelbaetapena.application.domain.Book
 import com.rafaelbaetapena.application.exceptions.FindBookByIdException
 import com.rafaelbaetapena.application.port.`in`.FindBookByIdUseCase
 import com.rafaelbaetapena.application.port.out.FindBookByIdAdapter
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.util.*
 import javax.inject.Singleton
 
 @Singleton
-class FindBookByIdUseCaseImpl(private val findBookByIdAdapter: FindBookByIdAdapter):
-        FindBookByIdUseCase {
+class FindBookByIdUseCaseImpl(
+        private val findBookByIdAdapter: FindBookByIdAdapter
+): FindBookByIdUseCase {
 
     override fun execute(id: UUID): Book {
-        return findBookByIdAdapter.execute(id) ?: throw FindBookByIdException()
+
+        log.info("$CLASS_NAME starting find book by id")
+
+        val book = findBookByIdAdapter.execute(id) ?: throw FindBookByIdException()
+
+        log.info("$CLASS_NAME finalized find book by id")
+
+        return book
+    }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(FindBookByIdUseCaseImpl::class.java)
+        private val CLASS_NAME = "[${FindBookByIdUseCaseImpl::class.java}]"
     }
 }
