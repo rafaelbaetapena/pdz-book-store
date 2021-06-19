851cf981bf01022e0afd10fabb6ea5003f5bbf27
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-06-18 23:06:34 -0300

    alterando o o nome para adapter

--- src/main/kotlin/com/rafaelbaetapena/application/port/in/impl/FindBookByIdUseCaseImpl.kt
+++ src/main/kotlin/com/rafaelbaetapena/application/port/in/impl/FindBookByIdUseCaseImpl.kt
@@ -3,15 +3,15 @@ package com.rafaelbaetapena.application.port.`in`.impl
 import com.rafaelbaetapena.application.domain.Book
 import com.rafaelbaetapena.application.exceptions.FindBookByIdException
 import com.rafaelbaetapena.application.port.`in`.FindBookByIdUseCase
-import com.rafaelbaetapena.application.port.out.FindBookByIdPort
+import com.rafaelbaetapena.application.port.out.FindBookByIdAdapter
 import java.util.*
 import javax.inject.Singleton
 
 @Singleton
-class FindBookByIdUseCaseImpl(private val findBookByIdPort: FindBookByIdPort):
+class FindBookByIdUseCaseImpl(private val findBookByIdAdapter: FindBookByIdAdapter):
         FindBookByIdUseCase {
 
     override fun execute(id: UUID): Book {
-        return findBookByIdPort.execute(id) ?: throw FindBookByIdException()
+        return findBookByIdAdapter.execute(id) ?: throw FindBookByIdException()
     }
 }
