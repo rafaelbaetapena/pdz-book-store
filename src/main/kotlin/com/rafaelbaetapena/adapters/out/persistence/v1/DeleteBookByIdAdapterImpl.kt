d5db1de68e88aeb3ce6cf4925510db0e538b8490
Rafael Baeta Pena <rafaelrbncp@gmail.com>
2021-06-21 21:20:42 -0300

    adicionando logs

--- src/main/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/DeleteBookByIdAdapterImpl.kt
+++ src/main/kotlin/com/rafaelbaetapena/adapters/out/persistence/v1/DeleteBookByIdAdapterImpl.kt
@@ -4,11 +4,22 @@ import com.rafaelbaetapena.adapters.out.persistence.v1.entities.BookEntity
 import com.rafaelbaetapena.adapters.out.persistence.v1.repositories.BookRepository
 import com.rafaelbaetapena.application.domain.Book
 import com.rafaelbaetapena.application.port.out.DeleteBookByIdAdapter
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class DeleteBookByIdAdapterImpl(
         private val bookRepository: BookRepository
 ): DeleteBookByIdAdapter {
     override fun execute(book: Book) {
+
+        log.info("$CLASS_NAME starting delete book by id")
+
         bookRepository.delete(BookEntity(book))
+
+        log.info("$CLASS_NAME finalized delete book by id")
+    }
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(DeleteBookByIdAdapterImpl::class.java)
+        private val CLASS_NAME = "[${DeleteBookByIdAdapterImpl::class.java}]"
     }
 }
--- src/main/kotlin/com/rafaelbaetapena/application/port/in/impl/DeleteBookByIdUseCaseImpl.kt
+++ src/main/kotlin/com/rafaelbaetapena/application/port/in/impl/DeleteBookByIdUseCaseImpl.kt
@@ -3,6 +3,8 @@ package com.rafaelbaetapena.application.port.`in`.impl
 import com.rafaelbaetapena.application.port.`in`.DeleteBookByIdUseCase
 import com.rafaelbaetapena.application.port.`in`.FindBookByIdUseCase
 import com.rafaelbaetapena.application.port.out.DeleteBookByIdAdapter
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.util.*
 import javax.inject.Singleton
 
@@ -13,7 +15,16 @@ class DeleteBookByIdUseCaseImpl(
 ): DeleteBookByIdUseCase {
 
     override fun execute(bookId: UUID) {
+
+        log.info("$CLASS_NAME starting delete book by id")
+
         val book = findBookByIdUseCase.execute(bookId)
         deleteBookByIdAdapter.execute(book)
+
+        log.info("$CLASS_NAME finalized delete book by id")
+    }
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(DeleteBookByIdUseCaseImpl::class.java)
+        private val CLASS_NAME = "[${DeleteBookByIdUseCaseImpl::class.java}]"
     }
 }
